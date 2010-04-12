b18f8e850dc81ca1ef419add677200132280695b
willrax <willrax@iinet.net.au>
2010-04-12 17:03:10 +1000

    added some stuff

---
+++ lib/google_viewer.rb
@@ -0,0 +1,28 @@
+class GoogleView
+
+  %w[CGI].map {|x| require x}
+
+  @@base_url = "http://docs.google.com/viewer?"
+  
+  def initialize(url)
+    @url = url
+    @viewer_url = @@base_url + "url=" + escaped(url)
+  end
+  
+  def url
+    @url
+  end
+  
+  def viewer_url
+    @viewer_url
+  end
+  
+  def open
+    system("open", "#{viewer_url}")
+  end
+
+  def escaped(url)
+    CGI::escape(url)
+  end
+
+end
