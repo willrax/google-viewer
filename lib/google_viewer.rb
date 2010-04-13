75d4fe7378cc23454c35efbda45e667be7d07aa9
willrax <willrax@iinet.net.au>
2010-04-13 12:52:13 +1000

    removed pointless methods

--- lib/google_viewer.rb
+++ lib/google_viewer.rb
@@ -1,28 +1,28 @@
-class GoogleView
+module Google
 
-  %w[CGI].map {|x| require x}
+  class GoogleViewer
 
-  @@base_url = "http://docs.google.com/viewer?"
-  
-  def initialize(url)
-    @url = url
-    @viewer_url = @@base_url + "url=" + escaped(url)
-  end
-  
-  def url
-    @url
-  end
-  
-  def viewer_url
-    @viewer_url
-  end
-  
-  def open
-    system("open", "#{viewer_url}")
-  end
+    %w[CGI].map {|x| require x}
 
-  def escaped(url)
-    CGI::escape(url)
+    @@base_url = "http://docs.google.com/viewer?"
+  
+    def initialize(url)
+      @url = url
+      @viewer_url = @@base_url + "url=" + escaped(url)
+    end
+  
+    def url
+      @url
+    end
+  
+    def viewer_url
+      @viewer_url
+    end
+
+    def escaped(url)
+        CGI::escape(url)
+    end
+
   end
 
 end
