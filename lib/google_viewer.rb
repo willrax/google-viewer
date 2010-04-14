df0dded79cef2e565c14344b6ce3f461fc2cf437
willrax <willrax@iinet.net.au>
2010-04-14 19:37:42 +1000

    fixed up initialize and adder reader mehod for viewer_url

--- lib/google_viewer.rb
+++ lib/google_viewer.rb
@@ -6,21 +6,18 @@ module Google
 
     @@base_url = "http://docs.google.com/viewer?"
   
+    attr_reader :url
+  
     def initialize(url)
       @url = url
-      @viewer_url = @@base_url + "url=" + escaped(url)
     end
-  
-    def url
-      @url
-    end
-  
+
     def viewer_url
-      @viewer_url
+      @viewer_url ||= @@base_url + "url=" + escaped(url)
     end
 
     def escaped(url)
-        CGI::escape(url)
+      CGI::escape(url)
     end
 
   end
